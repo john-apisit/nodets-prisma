622a91fdcda85daf40d049d667b2cf59a0389634
Apisit Sianglert <john.apisits@gmail.com>
2025-03-20 21:25:22 +0700

    Add controller

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
-import express, {Request, Response} from 'express'
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
 
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+const prisma = new PrismaClient();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Routes
+app.post(
+  "/v1/products",
+  async (
+    req: Request<any, any, { name: string; price: number }>,
+    res: Response
+  ) => {
+    const { name, price } = req.body;
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price,
+      },
+    });
+    res.json(product);
+  }
+);
 
-
+app.get("/v1/products", async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
